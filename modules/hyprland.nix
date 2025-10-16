eb60493e2511ca2ffaffa279cea6a4a2b8e45b57
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-10-16 16:01:24 +0300

    Hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -8,6 +8,26 @@
     package = pkgs.hyprland;
 
     settings = {
+"$mod" = "SUPER";
+"$alt" = "Alt";
+"$space" = "SPACE";
+"$control" = "CTRL";
+"shift" = "SHIFT";
+"$terminal" = "kitty";
+"$fileManager" = "thunar";
+"$menu" = "rofi"
+
+"exec-once" = [
+
+"waybar"
+];
+
+   "monitor" = [
+        "eDP-1, 1920x1080@144, 0x0, 1"
+      ];
+
+
+
       general = {
         "layout" = "dwindle";
         "gaps_in" = 5;
@@ -26,35 +46,61 @@
         };
       };
 
-      "monitor" = [
-        "eDP-1, 1920x1080@144, 0x0, 1"
-      ];
-          "$mod" = "SUPER";
-      "bind" =
+           "bind" =
         [
-          "$mod, T, exec, kitty"
+          "$mod, T, exec, $terminal"
           "$mod, Q, killactive"
           "$mod SHIFT, M, exit"
           "$mod, F, fullscreen"
           "$mod, V, togglefloating"
+	  "$mod, M, exit"
+	  "$mod, E, $fileManager"
+	  "$alt $space,,$menu -show drun"
+          
 
-          "$mod, left, movefocus, l"
+
+
+	  "$mod, left, movefocus, l"
           "$mod, right, movefocus, r"
           "$mod, up, movefocus, u"
           "$mod, down, movefocus, d"
 
-          "$mod SHIFT, left, movewindow, l"
+          
+
+
+
+	  "$mod SHIFT, left, movewindow, l"
           "$mod SHIFT, right, movewindow, r"
           "$mod SHIFT, up, movewindow, u"
           "$mod SHIFT, down, movewindow, d"
+
+	"$mod, 1 , workspace, 1"
+	"$mod, 2 , workspace, 2"
+	"$mod, 3 , workspace, 3"
+	"$mod, 4 , workspace, 4"
+	"$mod, 5 , workspace, 5"
+	"$mod, 6 , workspace, 6"
+	"$mod, 7 , workspace, 7"
+	"$mod, 8 , workspace, 8"
+	"$mod, 9 , workspace, 9"
+	"$mod, 0 , workspace, 10"
+
+
+
+	"$mod $shift, 1 ,  movetoworkspace, 1"
+	"$mod $shift, 2 ,  movetoworkspace, 2"
+	"$mod $shift, 3 ,  movetoworkspace, 3"
+	"$mod $shift, 4 ,  movetoworkspace, 4"
+	"$mod $shift, 5 ,  movetoworkspace, 5"
+	"$mod $shift, 6 ,  movetoworkspace, 6"
+	"$mod $shift, 7 ,  movetoworkspace, 7"
+	"$mod $shift, 8 ,  movetoworkspace, 8"
+	"$mod $shift, 9 ,  movetoworkspace, 9"
+	"$mod $shift, 0 ,  movetoworkspace, 10"
+
+
+
         ]
-        ++ lib.flatten (lib.genList (i:
-          let ws = builtins.toString (i + 1);
-          in [
-            "$mod, ${ws}, workspace, ${ws}"
-            "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
-          ]
-        ) 9);
     };
   };
 }
