298fdb37fc8a7eef3babee52cf490b890efc8545
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-10-28 18:33:21 +0300

    server common

--- server/configuration_server.nix
+++ server/configuration_server.nix
@@ -1,46 +1,26 @@
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
     [ 
       ./hardware-configuration.nix
+      ../modules/common.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "server-pc"; 
-   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-   time.timeZone = "Europe/Istanbul";
 
 
- i18n.defaultLocale = "en_US.UTF-8";
- console = {
- keyMap = "trq";   
- font = "latarcyrheb-sun32";
-   };
-
-
-nix.settings = {
-experimental-features = ["nix-command" "flakes" ];
-
-};
   
 
-environment.systemPackages = with pkgs; [
-git     
-neovim 
-terminus_font
-   ];
+networking.firewall = {
+  enable = true;
+  allowedTCPPorts = [ 22 80 443 8080];
+
+  allowedUDPPorts = [ 41641 ];
+};
 
 
-   services.openssh.enable = true;
-
-  system.stateVersion = "25.05"; 
+system.stateVersion = "25.05"; 
 
 }
-
