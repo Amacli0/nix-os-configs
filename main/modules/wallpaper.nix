ec85e561aa1fe05df72150bb9c32d11bc1f252e3
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-02 05:13:17 +0300

    wallpaper v6

--- main/modules/wallpaper.nix
+++ main/modules/wallpaper.nix
@@ -1,6 +1,7 @@
-{config,pkgs,...}:
+{ config, pkgs, ... }:
+
 {
- systemd.user.services.wallchange = {
+  systemd.user.services.wallchange = {
     Unit = {
       Description = "Change wallpaper periodically";
       After = [ "graphical-session.target" ];
@@ -9,32 +10,33 @@
     Service = {
       Type = "oneshot";
       ExecStart = ''
-        ${pkgs.bash}/bin/bash -c "
-          WALLPAPERS=\"$HOME/pictures"
-          RANDOM_WALL=$(find \"$WALLPAPERS\" -type f | shuf -n 1)
-          ${pkgs.swww}/bin/swww img \"$RANDOM_WALL\" --transition-type fade --transition-duration 2
-        "
+        WALLPAPERS="$HOME/pictures"
+        RANDOM_WALL=$(find "$WALLPAPERS" -type f | shuf -n 1)
+        ${pkgs.swww}/bin/swww img "$RANDOM_WALL" \
+          --transition-type fade \
+          --transition-duration 2
       '';
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
     };
   };
 
   systemd.user.timers.wallchange = {
-    Unit.Description = "Wallpaper change timer";
+    Unit = {
+      Description = "Wallpaper change timer";
+    };
+
     Timer = {
       OnBootSec = "5s";
-      OnUnitActiveSec = "30s";
+      OnUnitActiveSec = "30s"; # 30 saniye yerine 30 dakika önerilir
+      AccuracySec = "1s";
     };
-    Install.WantedBy = [ "timers.target" ];
+
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
   };
-
-
-
-
-
-
-
-
-
-
 }
 
