264c8ee0884d706627d4a5f4ab49821dbf949213
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-05 15:27:16 +0300

    HEDİ

--- main/home.nix
+++ main/home.nix
@@ -88,4 +88,18 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
+
+  systemd.user.services.hyprpolkitagent = {
+    Unit = {
+      Description = "Hyprland Polkit Authentication Agent";
+      After = ["graphical-session.target"];
+    };
+    Service = {
+      ExecStart = "${pkgs.hyprpolkitagent}/bin/hyprpolkitagent";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
 }
