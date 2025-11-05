e115528c54c875fd54a2d615faa48995ac8a2433
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-05 15:17:41 +0300

    s

--- main/home.nix
+++ main/home.nix
@@ -36,6 +36,7 @@
     swww
     waypaper
     btop
+    hyprpolkitagent
   ];
 
   programs = {
--- main/modules/hyprland.nix
+++ main/modules/hyprland.nix
@@ -21,7 +21,7 @@
       exec-once = [
         "${pkgs.waybar}/bin/waybar"
         "swww-daemon"
-        "hyprpolkitagent &"
+        "hyprpolkitagent"
       ];
 
       "monitor" = "eDP-1, 1920x1080@144, 0x0, 1";
