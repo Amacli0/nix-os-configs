fc2768d5d63d40a0f98fb574a848b426c3333e70
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-06 04:00:25 +0300

    root

--- server/configuration_server.nix
+++ server/configuration_server.nix
@@ -34,7 +34,7 @@
   #######################################
 
   sops.defaultSopsFile = ../secrets/main2.yaml;
-  sops.age.keyFile = "/home/server-pc/secrets/new_age_key.txt";
+  sops.age.keyFile = "/root/secrets/new_age_key.txt";
   sops.secrets = {
     "nextcloud_db_passwd" = {
       sopsFile = ../secrets/main.yaml;
