05c2e0ebc7c515bfea3cc6999020a0ba6b7b364e
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-08 14:36:42 +0300

    tekrar domain adi

--- server/services/podman.nix
+++ server/services/podman.nix
@@ -79,12 +79,7 @@ in {
 
           NEXTCLOUD_ADMIN_USER = "admin";
           NEXTCLOUD_ADMIN_PASSWORD = "admin"; # test için
-          NEXTCLOUD_TRUSTED_DOMAINS = [
-            "localhost"
-            "100.108.192.97"
-            "100.116.167.72"
-            "100.117.164.25"
-          ];
+          NEXTCLOUD_TRUSTED_DOMAINS = "localhost,100.108.192.97,100.116.167.72,100.117.164.25";
         };
         volumes = ["nextcloud_data:/var/www/html"];
         extraOptions = ["--network=nextcloud-net"];
