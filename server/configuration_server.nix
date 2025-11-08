ea7ef50b4bbb59e96f40b5c6df83116ba4a073b9
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-08 10:56:21 +0300

    olmayan dosya silindi + deney

--- server/configuration_server.nix
+++ server/configuration_server.nix
@@ -36,11 +36,8 @@
   sops.defaultSopsFile = ../secrets/main2.yaml;
   sops.age.keyFile = "/home/server-pc/secrets/new_age_key.txt";
   sops.secrets = {
-    "nextcloud_db_passwd" = {
-      sopsFile = ../secrets/main.yaml;
-      key = "POSTGRES_PASSWORD";
-      # NOT: 'key' kısmı, yaml dosyanın yapısına göre değişir.
-    };
+    sopsFile = ../secrets/main.yaml;
+    key = "POSTGRES_PASSWORD";
   };
   #######################################
   #              NETWORK                #
