47e99ab0cfad7b40640439482e222e9e837900ce
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-09 14:18:04 +0300

    Zapret

--- main/configuration.nix
+++ main/configuration.nix
@@ -48,6 +48,7 @@
   #######################################
   networking = {
     hostName = "Nixtilus";
+    nameservers = ["127.0.0.1" "::1"];
   };
   #######################################
   #            SERTİFİKA                #
@@ -59,6 +60,20 @@
   #            SERVİCES                 #
   #######################################
   services = {
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        listen_addresses = ["127.0.0.1:53" "[::1]:53"];
+      };
+    };
+
+    zapret = {
+      enable = true;
+      params = [
+        "--dpi-desync=fake"
+        "--dpi-desync-ttl=8"
+      ];
+    };
     #######################################
     #           EKRAN KARTI               #
     #######################################
