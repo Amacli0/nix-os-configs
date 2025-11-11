af7c61cffb5cf037a9f217bbd0d638ea0a548c62
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-11 16:48:34 +0300

    Deney

--- server/services/cloudflared.nix
+++ server/services/cloudflared.nix
@@ -5,17 +5,12 @@
 }: {
   services.cloudflared = {
     enable = true;
-
     tunnels."mini-ana" = {
       credentialsFile = "/var/lib/cloudflared/mini-tunnel.json";
-
-      # ingress bir attribute set olmalı: hostname → service
       ingress = {
-        "**tcp://terraria.deepshell.org**" = "tcp://100.66.96.36:7777";
+        "terraria.deepshell.org" = "tcp://100.66.96.36:7777";
+        "n8n.deepshell.org" = "http://100.66.36:8082";
       };
-
-      # default için 404 eklemek istersen cloudflared modülüne göre ayrı seçenek vardır.
-      # Eğer modül bunu destekliyorsa default = "http_status:404"; ekleyebilirsin
       default = "http_status:404";
     };
   };
