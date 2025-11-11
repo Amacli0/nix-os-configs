3e34d77c70f9b2de04e9332c437313fb2c714823
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-11 16:52:14 +0300

    offf

--- server/services/cloudflared.nix
+++ server/services/cloudflared.nix
@@ -9,7 +9,7 @@
       credentialsFile = "/var/lib/cloudflared/mini-tunnel.json";
       ingress = {
         "terraria.deepshell.org" = "tcp://100.66.96.36:7777";
-        "n8n.deepshell.org" = "http://100.66.36:8082";
+        "n8n.deepshell.org" = "http://100.66.96.36:8082";
       };
       default = "http_status:404";
     };
