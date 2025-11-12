6dafb69ac2c9b056684aab4d0dd7dd51aab6f9eb
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-12 22:05:16 +0300

    tekrar

--- main/configuration.nix
+++ main/configuration.nix
@@ -12,6 +12,7 @@
     ../common.nix
     ./hardware-configuration.nix
     ./modules/stylinx.nix
+    inputs.home-manager.nixosModules.home-manager
     ./modules/boot.nix
     ./modules/desktop.nix
     ./modules/fonts.nix
