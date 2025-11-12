eef3d1f61ece2f88633ee5a0dba10b6f957b2b8b
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-12 16:31:17 +0300

    geri alındı

--- main/home.nix
+++ main/home.nix
@@ -18,6 +18,7 @@
     ./home-modules/hyprland.nix
     ./home-modules/packages.nix
     inputs.stylix.homeManagerModules.stylix
+    ./home-modules/stylinx.nix
     ./home-modules/apps/obs.nix
     ./home-modules/apps/firefox.nix
     ./home-modules/apps/git.nix
