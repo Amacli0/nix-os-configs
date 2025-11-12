358cf102c1155fc7adf78b9eef28f88a36aec96d
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-13 00:01:20 +0300

    ly tekrardan

--- main/modules/desktop.nix
+++ main/modules/desktop.nix
@@ -9,28 +9,28 @@
   ...
 }: {
   services = {
-    libinput.enable = true;
+    xserver = {
+      displayManager.ly = {
+        enable = true;
+        settings = {
+          animate = true;
+          animation = "rain";
 
-    displayManager.ly = {
-      enable = true;
-      settings = {
-        animate = true;
-        animation = "rain";
+          bigclock = true;
+          clock = "%H:%M:%S";
+          date = "%a %d %b"; # Örn: Çar 12 Kas
 
-        bigclock = true;
-        clock = "%H:%M:%S";
-        date = "%a %d %b"; # Örn: Çar 12 Kas
+          message = "Bu ly harika değil mi hehehe...                    zamanimi boşa harciyorum.";
 
-        message = "Bu ly harika değil mi hehehe...                    zamanımı boşa harcıyorum.";
+          allow_root = false;
 
-        allow_root = false;
-
-        # Diğer Ayarlar
-        hide_f1_commands = true;
-        vt_switching = true;
+          # Diğer Ayarlar
+          hide_f1_commands = true;
+          vt_switching = true;
+        };
       };
     };
-
+    libinput.enable = true;
     pipewire = {
       enable = true;
       pulse.enable = true;
