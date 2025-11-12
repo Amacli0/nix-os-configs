27110e4fb25fe3da8ee5fd34b5fb79d26b9bbec3
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-12 16:38:28 +0300

    qt5 silindi yani geri alındı

--- main/modules/stylinx.nix
+++ main/modules/stylinx.nix
@@ -10,17 +10,4 @@
 }: {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-
-  stylix.targets = {
-    # Qt5 uygulamalarını temalaştırır.
-    qt5.enable = true;
-
-    # Qt6 uygulamalarını temalaştırır.
-    qt6.enable = true;
-  };
-
-  # home-manager'ın bu platform temalarını kullanması gerektiğini beyan ediyoruz
-  # Bu, QT_PLATFORM_THEME environment variable'ını ayarlar.
-  qt.enable = true;
-  qt.platformTheme = "qt5ct"; # qt5ct, Kvantum'u yöneten en yaygın Qt Config Tool'dur.
 }
