7f336d6c6d0d9cfed313a55060dab7f2d4bacc66
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-13 00:06:43 +0300

    Sadece deney for ly

--- main/modules/desktop.nix
+++ main/modules/desktop.nix
@@ -12,20 +12,6 @@
     displayManager.ly = {
       enable = true;
       settings = {
-        animate = true;
-        animation = "rain";
-
-        bigclock = true;
-        clock = "%H:%M:%S";
-        date = "%a %d %b"; # Örn: Çar 12 Kas
-
-        message = "Bu ly harika değil mi hehehe...                    zamanimi boşa harciyorum.";
-
-        allow_root = false;
-
-        # Diğer Ayarlar
-        hide_f1_commands = true;
-        vt_switching = true;
       };
     };
     libinput.enable = true;
