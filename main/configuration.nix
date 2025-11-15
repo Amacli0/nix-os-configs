09848f6d34969195832522f4cabff7a1bf4ab13f
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-15 23:13:17 +0300

    yazıcı

--- main/configuration.nix
+++ main/configuration.nix
@@ -33,11 +33,14 @@
     papirus-icon-theme
   ];
 
-services.printing.drivers = with pkgs; [ 
+services.printing = {
+ enable = true;
+    drivers = with pkgs; [ 
     gutenprint 
     brother-cups-wrapper # Brother'ın özel CUPS sarmalayıcısı
     brlaser            # Açık kaynak Brother sürücüsü (HL-20'ye destek verme olasılığı var)
   ];
+  };
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
 
--- main/modules/users.nix
+++ main/modules/users.nix
@@ -11,7 +11,7 @@
   users.users.deepshell = {
     shell = pkgs.zsh;
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "audio" "video" "libvirt" "kvm" "lp"];
+    extraGroups = ["wheel" "networkmanager" "audio" "video" "libvirt" "kvm" "lp" "lpadmin"];
   };
   users.users.softshell = {
     shell = pkgs.bash;
