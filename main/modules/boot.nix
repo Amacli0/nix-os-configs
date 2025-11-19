ace1ddda5a86edbce654e0d764c407dbe6611b33
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-19 23:40:29 +0300

    of

--- main/modules/boot.nix
+++ main/modules/boot.nix
@@ -8,13 +8,21 @@
   inputs,
   ...
 }: {
-  boot.kernelParams = [];
- boot.resumeDevice = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";}
-  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+boot.kernelPackages = pkgs.linuxPackages_latest;
+
+# Swap partition
+swapDevices = [
+  { device = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766"; }
+];
+
+# Hibernate (resume) cihazı
+boot.resumeDevice = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";
+
+# kernel parametreleri boş
+boot.kernelParams = [];
+
 
   boot.loader = {
     timeout = 15;
