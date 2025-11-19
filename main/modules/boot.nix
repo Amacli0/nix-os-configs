1669f0b3ab798072a042e8d8d7ad4be4a7e666e2
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-19 23:26:26 +0300

    js

--- main/modules/boot.nix
+++ main/modules/boot.nix
@@ -8,8 +8,11 @@
   inputs,
   ...
 }: {
+  boot.kernelParams = [];
  boot.resumeDevice = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";
-  swapDevices = [{device = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";}];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";}
+  ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
