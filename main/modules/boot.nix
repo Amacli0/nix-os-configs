48a6374324667921bd733e0b7ce0727729fb926b
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-11-19 23:17:21 +0300

    j

--- main/modules/boot.nix
+++ main/modules/boot.nix
@@ -10,7 +10,7 @@
 }: {
  boot.resumeDevice = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";
   swapDevices = [{device = "/dev/disk/by-uuid/2038d2a6-83ca-4c99-ae68-1cbab02ca766";}];
-boot.initrd.luks.devices."cryptroot".swap.uuid = "cd8cc7c6-4a37-4a5a-95a7-f40d6fa744b2";
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.loader = {
