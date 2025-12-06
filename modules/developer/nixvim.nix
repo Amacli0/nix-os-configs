2d3a3d56379ce9d137843a0a2004bac6d27f9201
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-12-07 02:32:22 +0300

    nixvim

--- modules/developer/nixvim.nix
+++ modules/developer/nixvim.nix
@@ -1,30 +1,28 @@
 {
   config,
-  lib,
   pkgs,
-  inputs,
   ...
 }: {
   programs.nixvim = {
     enable = true;
-    viAlias = true;
-    vimAlias = true;
+    globals.mapleader = " ";
 
     extraPackages = with pkgs; [
       ripgrep
       fd
 
-      nixfmt-rfc-style
+      go
+      gopls
+      gotools
+
+      bash-language-server
+
       black
-      stylua
-      terraform
+
+      alejandra
     ];
-
-    globals.mapleader = " ";
-
-    opts = {
+    options = {
       number = true;
-      relativenumber = true;
       expandtab = true;
       tabstop = 2;
       shiftwidth = 2;
@@ -35,203 +33,140 @@
       completeopt = "menu,menuone,noselect";
     };
 
-    colorschemes.gruvbox = {
-      enable = true;
-      settings.palette = "dark";
-    };
-
     plugins = {
       web-devicons.enable = true;
 
-      nvim-tree.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          pyright.enable = true;
+          terraformls.enable = true;
+          gopls.enable = true;
+          bashls.enable = true;
+          lua-ls.enable = true;
+        };
+      };
+      telescope = {
+        enable = true;
+      };
 
       gitsigns.enable = true;
 
+      nvim-tree.enable = true;
+      conform-nvim = {
+        enable = true;
+        formatOnSave = true;
+        formattersByFt = {
+          go = ["goimports"];
+          python = ["black"];
+          terraform = ["terraform_fmt"];
+          nix = ["alejandra"];
+        };
+      };
+
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+        };
+      };
       lualine = {
         enable = true;
         settings = {
           options.theme = "gruvbox";
         };
       };
-
-      telescope = {
-        enable = true;
-        extensions = {
-          fzf-native.enable = true;
-        };
-      };
-
-      lsp = {
-        enable = true;
-        servers = {
-          pyright.enable = true;
-          clangd.enable = true;
-          nixd.enable = true;
-          bashls.enable = true;
-          lua_ls.enable = true;
-          terraformls.enable = true;
-        };
-      };
-
-      lsp-signature = {
-        enable = true;
-        settings = {
-          bind = true;
-          handler_opts = {
-            border = "rounded";
-          };
-          hint_enable = true;
-          hint_prefix = "🔍 ";
-        };
-      };
-
-      conform-nvim = {
-        enable = true;
-        settings = {
-          format_on_save = {
-            timeout_ms = 500;
-            lsp_fallback = true;
-          };
-          formatters_by_ft = {
-            terraform = ["terraform_fmt"];
-            python = ["black"];
-            nix = ["alejandra"];
-            lua = ["stylua"];
-          };
-          formatters = {
-            nixfmt = {
-              command = "${pkgs.nixfmt-rfc-style}/bin/alejandra";
-            };
-            black = {
-              command = "${pkgs.black}/bin/black";
-            };
-            stylua = {
-              command = "${pkgs.stylua}/bin/stylua";
-            };
-            terraform_fmt = {
-              command = "${pkgs.terraform}/bin/terraform";
-              args = [
-                "fmt"
-                "-"
-              ];
-            };
-          };
-        };
-      };
-
-      hop = {
-        enable = true;
-        settings.keys = "etovxqpdygfblzhckisuran";
-      };
-
+      luasnip.enable = true;
       cmp = {
         enable = true;
         autoEnableSources = true;
+
         settings = {
           snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+
           mapping = {
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
             "<C-Space>" = "cmp.mapping.complete()";
-            "<C-p>" = "cmp.mapping.select_prev_item()";
-            "<C-n>" = "cmp.mapping.select_next_item()";
+            "<C-e>" = "cmp.mapping.close()";
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
             "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
           };
+
           sources = [
             {name = "nvim_lsp";}
             {name = "luasnip";}
-            {name = "buffer";}
             {name = "path";}
+            {name = "buffer";}
           ];
-        };
-      };
-
-      luasnip.enable = true;
-
-      neorg = {
-        enable = true;
-        settings = {
-          load = {
-            "core.defaults" = {
-              __empty = null;
-            };
-            "core.concealer" = {
-              __empty = null;
-            };
-            "core.dirman" = {
-              config = {
-                workspaces = {
-                  notes = "~/notes";
-                };
-                default_workspace = "notes";
-              };
-            };
-          };
-        };
-      };
-
-      zen-mode = {
-        enable = true;
-      };
-
-      treesitter = {
-        enable = true;
-        nixGrammars = true;
-        settings = {
-          highlight.enable = true;
-          indent.enable = true;
         };
       };
     };
 
+    ##########
+    #key maps#
+    ##########
     keymaps = [
       {
         mode = "n";
+        key = "<leader>w";
+        action = "<cmd>write<cr>";
+        options.desc = "Save File";
+      }
+      {
+        mode = "n";
+        key = "<leader>gd";
+        action = "<cmd>lua vim.lsp.buf.definition()<cr>";
+        options.desc = "Go to Definition";
+      }
+      {
+        mode = "n";
+        key = "K";
+        action = "<cmd>lua vim.lsp.buf.hover()<cr>";
+        options.desc = "Hover";
+      }
+      {
+        mode = "n";
         key = "<leader>e";
-        action = "<cmd>NvimTreeToggle<cr>";
-        options.desc = "Dosya ağacı";
+        action = "<cmd>lua vim.diagnostic.open_float()<cr>";
+        options.desc = "Diagnostics";
       }
       {
         mode = "n";
         key = "<leader>ff";
         action = "<cmd>Telescope find_files<cr>";
-        options.desc = "Dosya ara";
+        options.desc = "Telescope Find Files";
       }
       {
         mode = "n";
         key = "<leader>fg";
         action = "<cmd>Telescope live_grep<cr>";
-        options.desc = "Metin ara";
-      }
-      {
-        mode = "n";
-        key = "<leader>fm";
-        action = "<cmd>lua require('conform').format()<cr>";
-        options.desc = "Dosyayı Formatla";
+        options.desc = "Telescope Find Text";
       }
       {
         mode = "n";
         key = "<leader>gn";
         action = "<cmd>Gitsigns next_hunk<cr>";
-        options.desc = "Sonraki Git Değişikliği";
+        options.desc = "Git Next Hunk";
       }
       {
         mode = "n";
         key = "<leader>gp";
         action = "<cmd>Gitsigns prev_hunk<cr>";
-        options.desc = "Önceki Git Değişikliği";
+        options.desc = "Git Previous Hunk";
       }
       {
         mode = "n";
-        key = "<leader>zz";
-        action = "<cmd>ZenMode<cr>";
-        options.desc = "Odak Modu";
+        key = "<leader>nn";
+        action = "<cmd>NvimTreeToggle<cr>";
+        options.desc = "Toggle File Tree";
       }
       {
         mode = "n";
-        key = "<leader>w";
-        action = "<cmd>HopWord<cr>";
-        options.desc = "Hop: Kelimeye atla";
+        key = "<leader>fm";
+        action = "<cmd>ConformFormat<cr>";
+        options.desc = "Format Code";
       }
     ];
   };
