71d3921bb1d5abeeabc57a426d4dfa3cb5f3b807
Mehmet Şükrü Bilgiç <deepshell@proton.me>
2025-12-14 16:19:08 +0300

    nixvim boru

--- modules/developer/nixvim.nix
+++ modules/developer/nixvim.nix
@@ -68,9 +68,18 @@
           ];
         };
       };
-
-      nvimTsContextCommentstring = {
+      indent-blankline = {
         enable = true;
+        settings = {
+          indent = {
+            char = "│";
+          };
+          scope = {
+            enabled = true;
+            show_start = true;
+            show_end = true;
+          };
+        };
       };
 
       lualine = {
