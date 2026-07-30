5b1ab33743c0e7694914a9c2dcc9377fc7c33d57
Mehmet Sukru Bilgic <deepshell@proton.me>
2026-07-30 14:36:05 +0300

    as

--- modules/home/apps/foot.nix
+++ modules/home/apps/foot.nix
@@ -11,11 +11,9 @@
     enable = true;
     settings = {
       main = {
-        # Fallback olarak JetBrains Mono ekleyerek DejaVu uyarısını engelliyoruz
-        font = lib.mkForce "Fixedsys Excelsior:size=12, JetBrains Mono:size=11";
+        font = lib.mkForce "ProggyClean:size=13";
         pad = "8x8";
       };
-      # [colors] yerine [colors-dark] kullanıyoruz
       tweak = {
         font-monospace-warn = "no"; # Monospace uyarısını tamamen sessize al
       };
