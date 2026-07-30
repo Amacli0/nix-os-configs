f72a43a08381ea34aa402520f89136d6d8089edf
Mehmet Sukru Bilgic <deepshell@proton.me>
2026-07-30 21:35:03 +0300

    as

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -9,8 +9,8 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/windows-95.yaml";
-  stylix.polarity = "dark";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oceanicnext.yaml";
+  stylix.polarity = "light";
 
   stylix.targets.gtk.enable = false;
   stylix.targets.gnome.enable = false;
--- modules/home/apps/gtk.nix
+++ modules/home/apps/gtk.nix
@@ -8,24 +8,35 @@
   inputs,
   ...
 }: {
-  stylix.targets.gtk.enable = false;
   gtk = {
     enable = true;
+
+    # Frutiger Aero/Glossy hissiyatı veren aydınlık GTK teması
     theme = {
-      name = lib.mkForce "Chicago95";
-      package = lib.mkForce pkgs.chicago95;
+      name = "Adwaita";
+      package = pkgs.gnome-themes-extra;
     };
+
+    # Parlak ve renkli ikon seti (Yaru / Breeze veya Papirus-Light)
     iconTheme = {
-      name = lib.mkForce "Chicago95";
-      package = lib.mkForce pkgs.chicago95;
+      name = "Yaru";
+      package = pkgs.yaru-theme;
+    };
+
+    # Yumuşak ve modern imleç
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 24;
     };
   };
 
+  # İmlecin tüm sistemde varsayılan olması için
   home.pointerCursor = {
-    enable = true;
-    name = "Chicago95";
-    package = pkgs.chicago95;
+    name = "Bibata-Modern-Classic";
+    package = pkgs.bibata-cursors;
     size = 24;
     gtk.enable = true;
+    x11.enable = true;
   };
 }
